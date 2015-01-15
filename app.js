b24ccea13235d7b841c18eaba36c8cb61828b52c
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-15 15:21:18 +0100

    Update 2015-01-15T14:21:15.519Z

--- app.js
+++ app.js
@@ -15,28 +15,23 @@ var slogans = [
   "We Can Turn Potentials into Realities"
 ];
 
-var $part1 = $('#sloganPart1'), 
-    $part2 = $('#sloganPart2'),
-    $combined1 = $('#sloganPart1, #barSlogan1'),
-    $combined2 = $('#sloganPart2, #barSlogan2'),
-    $bar1 = $('#barSlogan1'),
-    $bar2 = $('#barSlogan2');
-    $sloganAll = $combined1.add($combined2);
-    $barsAll = $bar1.add($bar2);
+var $sloganAll = $('.slogans');
     
 var curSlogan = 0;
+var globalIndex = 0;
 
 var colors = ['#FF0000', '#00C34B', '#005FFF'];
-
-var colorsIndex = localStorage.getItem('colorsIndex') || 0;
+var colorsIndex = 0;
 var color = colors[colorsIndex];
 
 $('h1, h2, #barSlogan1, #barSlogan2').css('background', color);
 
-colorsIndex++;
-if (colorsIndex == 3) colorsIndex = 0;
+
 
 localStorage.setItem('colorsIndex', colorsIndex);
+
+var barHeight = 0;
+var windowHeight = $(window).height();
 
 function getBreakPoint(text) {
   if (text.indexOf("  ") != -1) {
@@ -83,22 +78,38 @@ function randomNegative() {
   return randomBoolean() ? -1 : 1;
 }
 
-function updateSlogan() {
+function setup() {
   $sloganAll.removeAttr('style');
+  barHeight = $sloganAll.find('.barSlogan1').height();
+  $sloganAll.remove();
+}
+
+function switchColors() {
+  colorsIndex++;
+  if (colorsIndex == 3) colorsIndex = 0;
+  color = colors[colorsIndex];
+}
+
+function newSlogan() {
+
+  var newSlogans = $sloganAll.clone();
   
-  var barHeight = $bar1.height();
-  var windowHeight = $(window).height();
+  var $part1 = newSlogans.find('.sloganPart1'), 
+      $part2 = newSlogans.find('.sloganPart2'),
+      $combined = newSlogans.find('.sloganPart1, .sloganPart2');
+
+  $('body').append(newSlogans);
   
   if (curSlogan == slogans.length) {
     curSlogan = 0;
   }
-
+  
   var text = slogans[curSlogan];
   var parts = getParts(text);
   
   $part1.html(parts[0]);
   $part2.html(parts[1]);
-  
+
   // Get width and reduce fontsize if necessary
   var width1 = $part1.width();
   var width2 = $part2.width();
@@ -110,61 +121,57 @@ function updateSlogan() {
     var reduceBy = screenWidth * 0.9 / largestWidth;
     
     // Normally font size is 2em
-    $sloganAll.css('font-size', Math.min(1.9, reduceBy * 2) + 'em');
+    newSlogans.children().css('font-size', Math.min(1.9, reduceBy * 2) + 'em');
   }
   
   // Modify position and rotation a bit
-  var rotate = randomBetween(2, 4);
+  var rotate = randomBetween(2, 5);
   var inverted = randomBoolean();
   
   if (inverted) rotate *= -1;
   
   var offsetFirst = (inverted) ? randomBetween(6, 13) : randomBetween(0, 4);
-  var offsetSecond = (inverted) ? randomBetween(30, 42) : randomBetween(35, 44);
   
-  // Calculate distance and correct if necessary
-  var distance = (offsetSecond * windowHeight / 100) - ((offsetFirst * windowHeight / 100) + barHeight);
+  $part1.css({
+    transform: 'rotate(' + rotate + 'deg) translate(-20vw, ' + offsetFirst + '%)',
+  });
   
-  // Distance as a ratio of the window height
-  var distanceRatio = distance / windowHeight;
+  $part2.css({
+    transform: 'rotate(' + -1 * rotate + 'deg) translate(-20vw, -2%)',
+  });
   
-  if (distanceRatio > 0.2) {
-    var desiredChange = distanceRatio / 0.2 * 1.3;
-    offsetFirst  *= desiredChange;
-    offsetSecond /= desiredChange;
+  $combined.css({
+    background: color,
+    width: '140vw'
+  });
+
+
+  
+  // Now also rotate and displace the whole group
+  if (globalIndex > 1) {
+    var rotate = randomBetween(-30, 30);
+    var top = randomBetween(0, 15);
     
-    offsetFirst  += desiredChange * 3;
-    offsetSecond += desiredChange * 3;
+    newSlogans.css({
+      transform: "rotate("+  rotate + "deg) translateY(" + top + "%)",
+    });
   }
-  
-  $combined1.css({
-    transform: 'rotate(' + rotate + 'deg)',
-    top: offsetFirst  + '%'
-  });
-  
-  $combined2.css({
-    transform: 'rotate(' + -1 * rotate + 'deg)',
-    top: offsetSecond  + '%'
-  });
-  
-  $bar1.css({
-    transform: 'rotate(' + rotate + 'deg) translate(-20vw, 0)'
-  });
-  
-  $bar2.css({
-    transform: 'rotate(' + -1 * rotate + 'deg) translate(-20vw, 0)'
-  });
-  
-  // Force their height to have the same as the other by filling with invisible Text
-  $barsAll.text('A').css({
-    color: color,
-    background: color
-  });
-  
+
   curSlogan++;
+  globalIndex++;
+  switchColors();
+  
+  while ($('.slogans').length > 20) {
+    $('.slogans').first().remove();
+  }
 }
 
+setup();
 
-window.setInterval(updateSlogan, 4000);
+window.setInterval(newSlogan, 4000);
 
-updateSlogan();
+newSlogan();
+
+window.onresize = function () {
+  windowHeight = $(window).height();
+};
